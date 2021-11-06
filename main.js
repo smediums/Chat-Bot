d914d0bf2bad423b0f12baebf42ccef814281338
smediums <smediums.me@gmail.com>
2021-11-07 01:21:55 +0300

    Complete redesign, got rid of unneeded code and features. finished.

--- main.js
+++ main.js
@@ -1,113 +1,7 @@
-// Finish Responses And possibel Inputs
-
-
-
 const header = document.querySelector('header');
 const nav = document.querySelector('nav');
-const chatIcon = document.querySelector('.chatIcon');
 const chatBox = document.querySelector('.chatBot');
 const chatContent = document.querySelector('.chatContent');
-const settings = document.querySelector('.fa-cog');
-
-
-
-//Toggle Function
-const toggleFunc = (trigger, eventType, item, classAdd, delay) => {
-    trigger.addEventListener(eventType, () => {
-        setTimeout(() => {
-            item.classList.toggle(classAdd);
-        }, delay);
-    });
-}
-
-toggleFunc(chatIcon, 'click', chatBox, 'openChat', 80);
-toggleFunc(settings, 'click', settings, 'settings', 0);
-
-
-// All Chat Icon & Chat Box events 
-
-const shrink = () => {
-    const closeChat = document.querySelector('.fa-times');
-    const minimize = document.querySelector('.fa-minus');
-    const minimizeDots = document.querySelector('.dots');
-    const dots = document.querySelectorAll('.dot');
-    const close2ndOptn = document.querySelector('.fa-undo');
-
-
-    //Chat Icon Switch Animation
-    chatIcon.addEventListener('click', () => {
-        chatIcon.classList.add('openChat');
-        setTimeout(() => {
-            close2ndOptn.classList.add('openChat');
-        }, 300);
-        minimizeDots.classList.remove('minimize');
-
-        //Close Chat Box With no Prompt
-        close2ndOptn.addEventListener('click', () => {
-            close2ndOptn.classList.remove('openChat');
-            setTimeout(() => {
-                chatIcon.classList.remove('openChat');
-            }, 300);
-            chatBox.classList.remove('openChat');
-            setTimeout(() => {
-                chatBox.classList.remove('minimize');
-            }, 200);
-
-            chatBox.lastElementChild.style.display = 'block';
-        })
-    });
-
-
-
-
-    //Close Chat Box
-    closeChat.addEventListener('click', () => {
-        if (confirm('Click OK to close the chat. Click Cancel to  cancel')) {
-            chatBox.classList.remove('openChat');
-            setTimeout(() => {
-                chatIcon.classList.remove('openChat');
-            }, 300);
-            close2ndOptn.classList.remove('openChat');
-            chatContent.innerHTML = '';
-        }
-    });
-
-
-    //Minimize Chat Box
-    minimize.addEventListener('click', () => {
-        chatBox.lastElementChild.style.display = 'none';
-        chatBox.classList.add('minimize');
-        setTimeout(() => {
-            minimizeDots.classList.add('minimize')
-        }, 300);
-
-        dots.forEach(dot => {
-            dot.classList.add('minimize');
-        });
-    });
-
-    //Chat Box Minimize animation
-    minimizeDots.addEventListener('click', () => {
-        chatBox.lastElementChild.style.display = 'block';
-
-        setTimeout(() => {
-            chatBox.classList.remove('minimize')
-        }, 100);
-
-        setTimeout(() => {
-            minimizeDots.classList.remove('minimize')
-        }, 0);
-
-        dots.forEach(dot => {
-            dot.classList.remove('minimize');
-        });
-
-    })
-
-
-}
-
-shrink();
 
 
 //Computer Responses and Response Triggers
@@ -136,13 +30,16 @@ const botReponses = () => {
     let todayDate = /date/i.test(userInput);
 
 
-
     //Fucntion for number equals Day of week name
     const theDate = () => {
-        if(dayOfWeek == 1){
-
+        let theDay;
+        if(dayOfWeek == 6){
+            // console.log('sat');
         }
     }
+
+    theDate();
+
 
     const botRespond = () => {
 
@@ -173,7 +70,7 @@ const botReponses = () => {
         if(todayDate){
             chatContent.innerHTML += `<div class="userText"><i class="fas fa-user-circle"></i><p>${userInput}</p>${curTime}</div>`;
             setTimeout(() => {
-                chatContent.innerHTML += `<div class="botResponse"><p class="reponse">Hello, please ask a question.</p>${curTime}</div>`;
+                chatContent.innerHTML += `<div class="botResponse"><p class="reponse">${theDay}</p>${curTime}</div>`;
                 chatContent.lastElementChild.scrollIntoView(false);
             }, 550);
             chatContent.lastElementChild.scrollIntoView(false);
@@ -188,7 +85,6 @@ const botReponses = () => {
 
     }
 
-
     botRespond();
 }
 
@@ -196,7 +92,7 @@ const botReponses = () => {
 let userInput = document.getElementById('userInput');
 userInput.addEventListener('keydown', (event) => {
 
-    if (event.keyCode == 13) {
+    if (event.key === 'Enter') {
         botReponses();
 
         const typingDiv = document.createElement('div');
